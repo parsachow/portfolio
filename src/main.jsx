dcb6d960a100bd6ff6f92218f7f268da0c1fc9a5
parsachow <parsakishwa06@gmail.com>
2023-09-09 16:59:03 -0400

    dependencies, components, page and routes setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import "vite/modulepreload-polyfill";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
-import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 )
